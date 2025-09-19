9537d5ca3564e9106c2a44b68043107fdd681cf3
Nick Fikatas <nick.fikatas@gmail.com>
2025-09-19 21:28:56 +0300

    Experimental Update #4

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/functions']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/functions']
+        }
+      }
+    }
+  }
 })
